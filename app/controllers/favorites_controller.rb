b8c82d2590b9f9a50db6d1f50bc504009931c52e
Ubuntu <ubuntu@ip-172-31-29-165.ap-northeast-1.compute.internal>
2025-10-10 06:44:09 +0000

    Initial Commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,9 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
 
-    redirect_to request.referer
+    respond_to do |format|
+      format.js # app/views/favorites/create.js.erb を呼び出す
+    end
   end
 
   def destroy
@@ -14,6 +16,8 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
 
-    redirect_to request.referer
+    respond_to do |format|
+      format.js # app/views/favorites/destroy.js.erb を呼び出す
+    end
   end
 end
